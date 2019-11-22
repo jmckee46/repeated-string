0f597c7efabfcb7cafe9417d4498f212bb14e04f
John McKee <jmckee3@mac.com>
2019-11-22 15:27:01 -0800

    working version

--- repeated-string.go
+++ repeated-string.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"strings"
+)
+
+func repeatedString(s string, n int64) int64 {
+	sLength := int64(len(s))
+
+	numOfAs := int64(strings.Count(s, "a"))
+
+	completeRepeats, remainder := n/sLength, n%sLength
+
+	completeRepeatAs := numOfAs * completeRepeats
+
+	remainingAs := int64(strings.Count(s[:remainder], "a"))
+
+	return completeRepeatAs + remainingAs
+}
+
 func main() {
 
 }
--- spec-repeated-string_test.go
+++ spec-repeated-string_test.go
@@ -3,5 +3,12 @@ package main
 import "testing"
 
 func TestRepeatedString1(t *testing.T) {
+	s := "aba"
+	n := int64(10)
+
+	numOfAs := repeatedString(s, n)
+	if numOfAs != 7 {
+		t.Errorf("got %d instead of 7", numOfAs)
+	}
 
 }
